6436dcbbdc33bc0ed4da9d883c3318dd79ed5551
jakedv24 <jakedveatch@gmail.com>
2017-07-23 16:05:55 -0500

    Update and rename master.html to homepage.php

---
+++ homepage.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<title>Corpys</title>
+		<style>
+		* {
+				margin: 0;
+				padding: 0;
+				border: 0;
+				outline: 0;
+				font-size: 100%;
+				vertical-align: baseline;
+				background: transparent;
+			}
+		</style>
+		<link href="styles.css" rel="stylesheet" type="text/css"/>
+	</head>
+	<body>
+		<header class="header1">
+			<ul>
+				<li class="logo" style="float:left;list-style-type:none;width:50%;">
+					<div style="width:50%;">
+						<a href="corpys.php">
+							<img src="corpyslogo.png" alt="Corpys" class="logo">
+						</a>
+					</div>
+				</li>
+				<li style="width:50%;height:78.4px;list-style-type:none;float:left;">
+					<div style="float:right;height:75px;width:250px;">
+						<a href="login.php" class="button1" style="float:left;margin:12px 1px;">Log In</a>
+						<a href="register.php" class="button2" style="float:right;margin:12px 1px;margin-right:2%;">Sign Up</a>
+					</div>
+				</li>
+					
+			</ul>
+		</header>
+		<header class="headerbreak"></header>
+		<header class="header2" style="color:#21EEBE;font-family:arial;text-align:center;padding:25px;font-size:145%">All of your subscriptions, all in one place.</header>
+		<div class="background" style="opacity:.4;">
+			<div class="paduppertext"></div>
+			<div class="textbox">
+				<div style="width:100%;height:17px;background:transparent;"></div>
+				<div style="min-height:92%;width:95%;color:grey;background-color:#D0D0D0;position:relative;margin:auto;padding-top:15px;opacity:2;">
+				</div>
+				
+			</div>
+			<div class="paduppertext"></div>
+			
+		</div>
+	</body>
+	<footer style="background:grey;min-height:200px;width:100%;border-radius:0px;border-color:black;border-width:3px;">
+	</footer>
+</html>
