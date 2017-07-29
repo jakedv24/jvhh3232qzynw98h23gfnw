4574cd3fde6fc1ff065514a7fd25849f4628a303
jakedv24 <jakedveatch@gmail.com>
2017-07-29 12:12:10 -0500

    Update corpys.php

--- corpys.php
+++ corpys.php
@@ -18,36 +18,48 @@
 	<body>
 		<header class="header1">
 			<ul>
-				<li style="float:left;list-style-type:none;width:50%;height:60px;
-					<div style="width:50%;">
-						<a href="corpys.php">
+				<li style="float:left;list-style-type:none;width:25%;height:100%;color:#21EEBE;font-family:arial;text-align:center;">
+					<div style="width:100%;font-size:100%;">
+						<a href="corpys.php" style="margin-bottom:3px;">
 							<img src="corpyslogo.png" alt="Corpys" class="logo">
 						</a>
 					</div>
 				</li>
-				<li style="width:50%;height:100%;list-style-type:none;float:left;">
-					<div style="float:right;height:100%;width:210px;">
-						<a href="login.php" class="button1" style="float:left;margin:8px 1px;">Log In</a>
-						<a href="register.php" class="button2" style="float:right;margin:8px 1px;margin-right:2%;">Sign Up</a>
+				<li style="float:left;list-style-type:none;width:50%;height:100%;color:#21EEBE;font-family:arial;text-align:center;padding:10px 0px 5px 0px;font-size:70%">
+					<div>All of your subscriptions, all in one place.</div>
+				</li>
+				<li style="width:25%;height:100%;list-style-type:none;float:left;">
+					<div style="float:right;height:100%;width:135px;">
+						<a href="login.php" class="button1" style="float:left;margin:4px 1px;font-size:50%;">Log In</a>
+						<a href="register.php" class="button2" style="float:right;margin:4px 1px;margin-right:4%;font-size:50%;">Sign Up</a>
 					</div>
 				</li>
 					
 			</ul>
 		</header>
-		<header class="headerbreak"></header>
-		<header class="header2" style="color:#21EEBE;font-family:arial;text-align:center;padding:5px 0px 5px 0px;font-size:110%">All of your subscriptions, all in one place.</header>
-		<div class="background" style="opacity:.4;">
+		<div class="background" style="opacity:1;">
 			<div class="paduppertext"></div>
 			<div class="textbox">
 				<div style="width:100%;height:17px;background:transparent;"></div>
-				<div style="min-height:92%;width:95%;color:grey;background-color:#D0D0D0;position:relative;margin:auto;padding-top:15px;opacity:2;">
+				<div style="min-height:92%;width:95%;color:black;background-color:#D0D0D0;position:relative;margin:auto;padding-top:15px;opacity:1;">
+					<ul>
+						<li style="width:49.75%;height:100%;list-style-type:none;float:left;">
+							<div class="text_left_home">test</div>
+						</li>
+						<li style="min-width:.5%;min-height:250px;background-color:black;float:left;list-style-type:none;">
+							<div style="min-height:100%;min-width:100%;"></div>
+						</li>
+						<li style="width:49.75%;height:100%;list-style-type:none;float:left;">
+							<div class="text_right_home">test</div>
+						</li>
+					</ul>
 				</div>
 				
 			</div>
-			<div class="paduppertext"></div>
+			
 			
 		</div>
 	</body>
-	<footer style="background:grey;min-height:200px;width:100%;border-radius:0px;border-color:black;border-width:3px;">
+	<footer style="background:grey;min-height:100px;width:100%;border-radius:0px;border-color:black;border-width:3px;">
 	</footer>
 </html>
