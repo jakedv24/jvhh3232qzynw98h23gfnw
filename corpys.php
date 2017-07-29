7c48950fa53ad33adbcf1ab808b3c3bbebf2467c
jakedv24 <jakedveatch@gmail.com>
2017-07-29 13:02:16 -0500

    Update and rename master.html to corpys.php

---
+++ corpys.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<title>Corpys</title>
+		<style>
+		* {
+				margin: 0;
+				padding: 0;
+				border: 0;
+				outline: 0;
+				font-size: 100%;
+				vertical-align: baseline;
+				background: transparent;
+			}
+		</style>
+		<link href="styles.css" rel="stylesheet" type="text/css"/>
+	</head>
+	<body>
+		<header class="header1">
+			<ul>
+				<li style="float:left;list-style-type:none;width:50%;height:100%;color:#21EEBE;font-family:arial;text-align:center;">
+					<div style="width:100%;font-size:100%;">
+						<a href="corpys.php" style="margin-bottom:3px;">
+							<p class="logo"><u>Corpys</u></p>
+						</a>
+					</div>
+				</li>
+				<li style="width:50%;height:100%;list-style-type:none;float:left;">
+					<div style="float:right;height:100%;width:135px;">
+						<a href="login.php" class="button1" style="float:left;margin:5px 1px;font-size:50%;">Log In</a>
+						<a href="register.php" class="button2" style="float:right;margin:5px 1px;margin-right:4%;font-size:50%;">Sign Up</a>
+					</div>
+				</li>
+					
+			</ul>
+		</header>
+		<div class="grey_background" style="min-height:500px;width:100%;background:#F7F6F7;">
+			<h1 style="text-align:center;font-family:Arial;padding:20px;color:#21EEBE;text-weight:.2;">All of your subscriptions, all in one place.</h1>
+		</div>
+		<div class="white_background" style="min-height:500px;width:100%;background:white;"></div>
+	</body>
+	<footer style="background:grey;min-height:100px;width:100%;border-radius:0px;border-color:black;border-width:3px;">
+	</footer>
+</html>
